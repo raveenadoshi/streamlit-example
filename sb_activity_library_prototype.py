e9d14bea9b7300f6ac7b26c0bc05013bb0f88c0a
raveenadoshi <47232142+raveenadoshi@users.noreply.github.com>
2024-05-12 01:55:52 -0700

    Update web app and content

--- sb_activity_library_prototype.py
+++ sb_activity_library_prototype.py
@@ -3,17 +3,17 @@ import streamlit as st
 import streamlit.components.v1 as components
 from pandas.api.types import (
     is_categorical_dtype,
-    is_datetime64_any_dtype,
     is_numeric_dtype,
-    is_object_dtype,
 )
 
-st.title("Welcome to the Six Bricks Activity Library!")
+st.image("Six Bricks Banner.png")
 
-st.write("""Use the filters on the left-hand side of this screen to find activities that suit the developmental areas and subjects you're interested in browsing activities for.
+st.title("Six Bricks Activity Library")
+
+st.write("""Welcome to the Six Bricks Library Activity! Use the filters on the left-hand side of this page to find Six Bricks activities that align with the developmental areas or subjects you're interested in.
         """)
 
-st.write("""To learn more, visit [Care for Education](https://www.carefored.co.za) -- home of Six Bricks.
+st.write("""To learn more about Six Bricks, visit [Care for Education](https://www.carefored.co.za) -- home of the Six Bricks methodology.
         """)
 
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
@@ -28,34 +28,17 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
     
     with st.sidebar:
-        modify = st.checkbox("Add filters")
-    
-        if not modify:
-            return df
     
         df = df.copy()
-    
-        # Try to convert datetimes into a standard format (datetime, no timezone)
-        for col in df.columns:
-            if is_object_dtype(df[col]):
-                try:
-                    df[col] = pd.to_datetime(df[col])
-                except Exception:
-                    pass
-    
-            if is_datetime64_any_dtype(df[col]):
-                df[col] = df[col].dt.tz_localize(None)
-    
-        modification_container = st.container()
-    
-        with modification_container:
-            to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        
+        with st.container():
+            to_filter_columns = st.multiselect("Select your filters:", df.columns,("Subject areas","Key skill areas"))
             for column in to_filter_columns:
                 left, right = st.columns((1, 20))
                 # Treat columns with < 10 unique values as categorical
                 if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
                     user_cat_input = right.multiselect(
-                        f"Values for {column}",
+                        f"Select {column}:",
                         df[column].unique(),
                         default=list(df[column].unique()),
                     )
@@ -72,33 +55,22 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                         step=step,
                     )
                     df = df[df[column].between(*user_num_input)]
-                elif is_datetime64_any_dtype(df[column]):
-                    user_date_input = right.date_input(
-                        f"Values for {column}",
-                        value=(
-                            df[column].min(),
-                            df[column].max(),
-                        ),
-                    )
-                    if len(user_date_input) == 2:
-                        user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                        start_date, end_date = user_date_input
-                        df = df.loc[df[column].between(start_date, end_date)]
+
                 else:
                     user_text_input = right.text_input(
-                        f"Substring or regex in {column}",
+                        f"Type below to search by {column}:",
                     )
                     if user_text_input:
                         df = df[df[column].astype(str).str.contains(user_text_input)]
     return df
 
 df = pd.read_csv("penguins.csv")
-st.dataframe(filter_dataframe(df))
+df_filtered = filter_dataframe(df)
 
-from pandas.api.types import (
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype,
-
+st.dataframe(
+    df_filtered,
+    column_config={
+        "Link to activity": st.column_config.LinkColumn("Link to activity")
+    },
+    hide_index=True,
 )
